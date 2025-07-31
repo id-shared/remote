73115f24bb5448b0c2c01672d1861989cdf88bb9
id-shared <it.is.a.shared@gmail.com>
2025-07-30 20:32:15 -0700

    changes

--- x/_.rs
+++ x/_.rs
@@ -54,16 +54,16 @@ pub fn main() {
     fn check(z: u32) -> bool {
       let (n1, n2, n3, _) = color(z);
 
-      match n1 >= 239 {
-        T => match n3 >= 239 {
-          T => match 8 >= n1.abs_diff(n3) {
+      match n1 >= 191 {
+        T => match n3 >= 191 {
+          T => match 16 >= n1.abs_diff(n3) {
             T => match n1 >= n3 {
               T => match n3 >= n2 {
-                T => n3.abs_diff(n2) >= 32,
+                T => n3.abs_diff(n2) >= 64,
                 _ => F,
               },
               _ => match n1 >= n2 {
-                T => n1.abs_diff(n2) >= 32,
+                T => n1.abs_diff(n2) >= 64,
                 _ => F,
               },
             },
