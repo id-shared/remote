2cf1cb193ae06988c7583164b9c4549c22f82922
id-shared <it.is.a.shared@gmail.com>
2025-08-09 14:44:53 -0700

    changes

--- _/xyloid/d2.rs
+++ _/xyloid/d2.rs
@@ -137,11 +137,11 @@ pub fn io(k: VIRTUAL_KEY, n: u16, z: &Device) -> bool {
 }
 
 pub fn mkcode(key: VIRTUAL_KEY) -> u16 {
-  (unsafe { MapVirtualKeyW(u32::from(key.0), windows::Win32::UI::Input::KeyboardAndMouse::MAP_VIRTUAL_KEY_TYPE(0)) }) as u16
+  u16::try_from(unsafe { MapVirtualKeyW(u32::from(key.0), windows::Win32::UI::Input::KeyboardAndMouse::MAP_VIRTUAL_KEY_TYPE(0)) }).unwrap()
 }
 
 pub fn is(key: VIRTUAL_KEY) -> bool {
-  (unsafe { GetAsyncKeyState(i32::from(key.0)) }) as u32 & 0x8000u32 != 0
+  u32::from(unsafe { GetAsyncKeyState(i32::from(key.0)) }.unsigned_abs()) & 0x8000u32 != 0
 }
 
 pub const F: bool = false;
